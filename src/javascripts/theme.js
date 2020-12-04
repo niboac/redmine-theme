437ade272e4ae2da0c884f24cf7a90e091899728
caobin <caobin2@kingsoft.com>
2020-12-04 17:28:58 +0800

    v1.0

---
+++ src/javascripts/theme.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+  localStorage.getItem("myName");
+
+  document.querySelectorAll('.issues.list tr td.status').forEach(item => {
+
+    if ($(item).text() == 'New') {
+      $(item).parent().find('a').css('color', 'green');
+      $(item).css('color', 'green');
+    }
+
+    if ($(item).text() == 'Resolved') {
+      $(item).parent().find('a').css('color', '#B859DB');
+      $(item).css('color', '#B859DB');
+    }
+
+    if ($(item).text() == 'In Progress') {
+      $(item).parent().find('a').css('color', '#5EC6DB');
+      $(item).css('color', '#5EC6DB');
+    }
+
+    if ($(item).text() == 'Feedback') {
+      $(item).parent().find('a').css('color', '#aaa');
+      $(item).css('color', '#aaa');
+    }
+
+    if ($(item).text() == 'Rejected') {
+      $(item).parent().find('a').css('text-decoration', 'line-through');
+      $(item).parent().find('a').css('color', '#666');
+      $(item).css('text-decoration', 'line-through');
+    }
+
+    if ($(item).text() == 'Closed') {
+      $(item).parent().find('a').css({ 'color': '#eee', 'background': '#666' });
+    }
+
+
+  })
+
+  document.querySelectorAll('.issues.list tr td.tracker').forEach(item => {
+
+    if ($(item).text() == 'Bug') {
+      $(item).text('🐞');
+    }
+  })
+
+  document.querySelectorAll('.issues.list tr td.assigned_to a').forEach(item => {
+
+    if ($(item).text() == localStorage.getItem("myName")) {
+      $(item).html("<span style='color:red;'>@我️</span>");
+    }
+  })
+
+  document.querySelectorAll('.issues.list tr td.priority').forEach(item => {
+    if ($(item).text() == 'High') {
+      $(item).css('color', '#DB5959');
+    }
+
+    if ($(item).text() == 'Urgent') {
+      $(item).css('color', 'red');
+    }
+  })
+
+
+})
